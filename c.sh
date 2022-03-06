0d97febe6a3de63ad463cdf5fd6cf3c04512017d
odevdeneme7896354125 <101044061+odevdeneme7896354125@users.noreply.github.com>
2022-03-06 15:03:58 +0300

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,8 @@
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+
+sudo apt-get install --no-install-recommends chromium-browser
+
+cd /etc/xdg/openbox
+
+sudo nano autostart
+
