a4491c273ee826369b2a040ed716a0c535015cd6
odevdeneme7896354125 <101044061+odevdeneme7896354125@users.noreply.github.com>
2022-03-06 13:42:48 +0300

    Create a.sh

---
+++ a.sh
@@ -0,0 +1,15 @@
+sudo apt-get upgrade
+
+wget https://unofficial-builds.nodejs.org/download/release/v14.13.0/node-v14.13.0-linux-armv6l.tar.xz
+
+tar xvfJ node-v14.13.0-linux-armv6l.tar.xz
+
+sudo cp -R node-v14.13.0-linux-armv6l/ * /usr/local
+
+
+git clone https://github.com/MichMich/MagicMirror
+
+cd MagicMirror
+
+sudo npm install
+
